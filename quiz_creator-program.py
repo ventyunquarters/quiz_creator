237ca0a4757238d7503fb1870fa579f8fd4ff966
unknown <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-04-11 22:32:51 +0800

    Use the for loop to loop at each questions and split them so I can cut as well the prefixes where I remove the mark for the Q, A, and C

--- quiz_creator-program.py
+++ quiz_creator-program.py
@@ -8,6 +8,16 @@ def load_quiz(filename):
     with open(filename, 'r') as file:
         raw_entries = file.read().strip().split('\n\n') #Splits the tet at every double line for questions
 
+    quiz_data = []
+    for entry in raw_entries:
+        lines = entry.strip().split('\n')
+        #Extract the question, correct answer, nad choices
+        question  = lines[0][3:] # Skip the "Q: "
+        answer  = lines[1][3:] # Skip the "A: "
+        choices  = lines[2][3:].split(';') #Split choices by semicolon
+        #Store the question, correct answer, and choices in a list
+        quiz_data.append((question, answer, choices))
+    return quiz_data
 
 #Ask one question and return the score earned and correctness
 
