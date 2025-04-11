3554f1fec1aa01b29a06c3abdf4d4f43afee44c1
unknown <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-04-11 22:05:27 +0800

    Outline pseudocode for idea.

---
+++ quiz_creator-program.py
@@ -0,0 +1,9 @@
+#Define the quiz function to load it
+
+#Ask one question and return the score earned and correctness
+
+#Add a timer to give bonuses for early answers
+
+#Stop the game when there's three incorrect answers
+
+#Record the highscore
